fa32f4802282d41b630a9f8eb7e2fb883d705a01
yukii1004 <yukii1004.dev@gmail.com>
2023-11-01 06:10:34 +0530

    handled all routes and frontend almost done

--- app.py
+++ app.py
@@ -6,9 +6,17 @@ app = Flask(__name__, static_url_path='', static_folder='static', template_folde
 def home():
     return render_template('home.html')
 
+@app.route('/verbalyze')
+def verbalyze():
+    return render_template('verbalyze.html')
+
+@app.route('/download')
+def download():
+    return render_template('download.html')
+
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
